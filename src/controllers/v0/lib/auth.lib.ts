b06bf0ca83ffe6e7316bbb2096c567ed2b4e6b85
VarunRaj7 <rvarunraj1@gmail.com>
2020-06-09 11:23:00 -0400

    adding lib dir and its changes

---
+++ src/controllers/v0/lib/auth.lib.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import * as c from '../../../config/config';
+import { NextFunction } from 'connect';
+import * as jwt from 'jsonwebtoken';
+
+export function requireAuth(req: Request, res: Response, next: NextFunction) {
+  if (!req.headers || !req.headers.authorization) {
+    return res.status(401).send({ message: 'No authorization headers.' });
+  }
+
+  const tokenBearer = req.headers.authorization.split(' ');
+  if (tokenBearer.length != 2) {
+    return res.status(401).send({ message: 'Malformed token.' });
+  }
+
+  const token = tokenBearer[1];
+  return jwt.verify(token, c.config.jwt.secret, (err, decoded) => {
+    if (err) {
+      return res
+        .status(500)
+        .send({ auth: false, message: 'Failed to authenticate.' });
+    }
+    return next();
+  });
+}
